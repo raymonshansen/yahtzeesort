925a59bcb5ef02918adae45227f4d7e26c5beafe
Raymon <raymon.s.hansen@gmail.com>
2017-02-14 22:53:05 +0100

    Rewrote to work with a struct containing the array itself and an int holding its size

--- yahtzeesort.c
+++ yahtzeesort.c
@@ -3,15 +3,21 @@
 #include <math.h>
 #include <time.h>
 
+// Struct to hold an array and its size
+typedef struct container container_t;
+struct container {
+	int *array;
+	int size;
+};
 
 /*
   Utility function to print an array of integers
 */
-void print_array(int *array, int N){
+void print_array(container_t *array){
 	int i;
 	printf("\n");
-	for(i = 0; i < N; i++){
-		printf("%d\t", array[i]);
+	for(i = 0; i < array->size; i++){
+		printf("%d\t", array->array[i]);
 	}
 	printf("\n");
 }
@@ -43,14 +49,15 @@ int randint(int from, int to){
   by Richard Durstenfeld. It does an 
   in-place shuffle of the array.
 */
-void shuffle(int *array, int N){
+void shuffle(container_t *array){
 	int i, j, temp;
-	for(i = 0; i <= N - 2; i++){
-		j = randint(i, N-1);
-		// Swap the two values
-		temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
+	for(i = 0; i <= array->size - 2; i++){
+		// find a random index in the array...
+		j = randint(i, array->size-1);
+		// ...and swap this with the current index
+		temp = array->array[i];
+		array->array[i] = array->array[j];
+		array->array[j] = temp;
 	}
 }
 
@@ -61,22 +68,24 @@ void shuffle(int *array, int N){
   rewritten to omit the missing part. Takes
   the array and its size(N) as input.
 */
-void extract_sequence(int *array, int N){
+void extract_sequence(container_t *array){
+	// Declaring the conters
 	int i;
 	int greatest = 1;
 	int size = 1;
 	int j = 0; 
 	int startindex = 0;
-	// Iterate through the array
-	for(i = 0; i < (N-greatest); i++){
+	// Iterate through the array.
+	for(i = 0; i < (array->size-greatest); i++){
 		size = 1;
 		j = i;
-		// Check for increasing consequtive values, or falling off
-		while((j+1 < N) && (array[j+1] >= array[j])){
+		// Check for increasing consequtive values, 
+		// or falling off the edge of the array.
+		while((j+1 < array->size) && (array->array[j+1] >= array->array[j])){
 			size++;
 			j++;
 		}
-		// If new greatest conseq. sequence was found,
+		// If new greatest consequtive sequence was found,
 		// store the size and starting position
 		if(size >= greatest){
 			startindex = i;
@@ -85,31 +94,35 @@ void extract_sequence(int *array, int N){
 	}
 	// Extract the wanted sub-array
 	printf("Startindex: %d Size of subarray: %d\n",startindex, greatest);
-	int subarray[greatest];
+	container_t *subarray = malloc(sizeof(container_t));
+	subarray->array = malloc(sizeof(int)*greatest);
+	subarray->size = greatest;
 	int k = 0;
 	int l = startindex;
-	while(k < greatest){
-		subarray[k] = array[l];
+	while(k < subarray->size){
+		subarray->array[k] = array->array[l];
 		k++;
 		l++;
 	}
-	print_array(subarray, greatest);
+	print_array(subarray);
 
 	// Reassemble the remaining
-	int *newarray = malloc(sizeof(int)*(N-greatest));
+	container_t *newarray = malloc(sizeof(container_t));
+	newarray->array = malloc(sizeof(int)*(array->size-greatest));
+	newarray->size = array->size-greatest;
 	// Fill before
 	for(i = 0; i < startindex; i++){
-		newarray[i] = array[i];
+		newarray->array[i] = array->array[i];
 	}
 	// Fill after
 	i = startindex + greatest;
 	j = startindex;
-	while(i < N){
-		newarray[j] = array[i];
+	while(i < array->size){
+		newarray->array[j] = array->array[i];
 		j++;
 		i++;
 	}
-	print_array(newarray, N-greatest);
+	print_array(newarray);
 
 }
 
@@ -127,22 +140,23 @@ int main(int argc, char const *argv[])
 	if(argc != 2){
 		printf("Usage:\nyahtzeesort <size of array>");
 		return 1;
-	} 
-	int size = atoi(argv[1]);
-	printf("Filling array of %d with random numbers\n", size);
-	int *array = malloc(sizeof(int)*size);
-	// Populate the array with random int.
-	int i;
+	}
+	container_t *array = malloc(sizeof(container_t));
+	array->size = atoi(argv[1]);
+	printf("Filling array of %d with random numbers\n", array->size);
+	array->array = malloc(sizeof(int)*array->size);
 	// Seed the generator with time
 	srand((unsigned int)time(NULL));
-	printf("Original array\n");
-	for(i = 0; i < size; i++){
-		array[i] = randint(0, size);
+	// Populate the array with random int.
+	int i;
+	for(i = 0; i < array->size; i++){
+		array->array[i] = randint(0, array->size);
 	}
-	print_array(array, size);
-	shuffle(array, size);
-	print_array(array, size);
+	printf("Original array\n");
+	print_array(array);
+	shuffle(array);
+	print_array(array);
 	// Find the largest sequential part of the array
-	extract_sequence(array, size);
+	extract_sequence(array);
 	return 0;
 }
