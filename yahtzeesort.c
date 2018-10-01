f339659a27a2e25a68bbd511e762df37e4746594
raymonshansen <raymon.s.hansen@gmail.com>
2018-10-01 20:50:16 +0200

    Fix. Cleanup and rename.

--- yahtzeesort.c
+++ yahtzeesort.c
@@ -3,23 +3,30 @@
 #include <math.h>
 #include <time.h>
 
+
 // Struct to hold an array and its size
-typedef struct container container_t;
-struct container {
+typedef struct vector array_t;
+struct vector {
 	int *array;
 	int size;
 };
 
 /*
-  Utility function to print an array of integers
+  Allocate a new numbered array.
 */
-void print_array(container_t *array){
-	int i;
-	printf("\n");
-	for(i = 0; i < array->size; i++){
-		printf("%d\t", array->array[i]);
-	}
-	printf("\n");
+array_t* new_array(int size){
+    array_t* new = malloc(sizeof(array_t) * sizeof(*new));
+    new->size = size;
+    new->array = malloc(sizeof(int) * size);
+    return new;
+}
+
+/*
+  Deallocate a vector struct properly.
+*/
+void dealloc_array(array_t* vector){
+    free(vector->array);
+    free(vector);
 }
 
 /*
@@ -29,18 +36,29 @@ void print_array(container_t *array){
   Return values can include BOTH input values!
 */
 int randint(int from, int to){
-	int low = 0;
-	int high = 0;
-	// Health checks
-	if(from < to){
-		low = from;
-		high = to + 1;
-	} else {
-		low = to;
-		high = from + 1;
-	}
+    int low = 0;
+    int high = 0;
+    // Health checks
+    if(from < to){
+        low = from;
+        high = to + 1;
+    } else {
+        low = to;
+        high = from + 1;
+    }
+    return (rand() % (high - low)) + low;
+}
 
-	return (rand() % (high - low)) + low;
+/*
+  Utility function to print an array of integers
+*/
+void print_array(array_t *array){
+    int i;
+    printf("\n");
+    for(i = 0; i < array->size; i++){
+        printf("%d\t", array->array[i]);
+    }
+    printf("\n");
 }
 
 /*
@@ -49,116 +67,152 @@ int randint(int from, int to){
   by Richard Durstenfeld. It does an
   in-place shuffle of the array.
 */
-void shuffle(container_t *array){
-	int i, j, temp;
-	for(i = 0; i <= array->size - 2; i++){
-		// find a random index in the array...
-		j = randint(i, array->size-1);
-		// ...and swap this with the current index
-		temp = array->array[i];
-		array->array[i] = array->array[j];
-		array->array[j] = temp;
-	}
+void shuffle(array_t *array){
+    int i, j, temp;
+    // Find a random index in the array and swap with current
+    for(i = 0; i <= array->size - 2; i++){
+        j = randint(i, array->size-1);
+        temp = array->array[i];
+        array->array[i] = array->array[j];
+        array->array[j] = temp;
+    }
 }
 
 /*
   Function to find the largest sequential
-  part of an array. This is then extracted
-  and returned. The remaining array is
-  rewritten to omit the missing part. Takes
-  the array and its size(N) as input.
+  part of an array, including the index where it starts.
 */
-container_t *extract_sequence(container_t *array){
-	// Declaring the conters
-	int i;
-	int greatest = 1;
-	int size = 1;
-	int j = 0;
-	int startindex = 0;
-	// Iterate through the array.
-	for(i = 0; i < (array->size-greatest); i++){
-		size = 1;
-		j = i;
-		// Check for increasing consequtive values,
-		// or falling off the edge of the array.
-		while((j+1 < array->size) && (array->array[j+1] >= array->array[j])){
-			size++;
-			j++;
-		}
-		// If new greatest consequtive sequence was found,
-		// store the size and starting position
-		if(size >= greatest){
-			startindex = i;
-			greatest = size;
-		}
-	}
-	// Extract the wanted sub-array
-	printf("Startindex: %d Size of subarray: %d\n",startindex, greatest);
-	container_t *subarray = malloc(sizeof(container_t));
-	subarray->array = malloc(sizeof(int)*greatest);
-	subarray->size = greatest;
-	int k = 0;
-	int l = startindex;
-	while(k < subarray->size){
-		subarray->array[k] = array->array[l];
-		k++;
-		l++;
-	}
-	print_array(subarray);
-
-	// Reassemble the remaining
-	container_t *newarray = malloc(sizeof(container_t));
-	newarray->array = malloc(sizeof(int)*(array->size-greatest));
-	newarray->size = array->size-greatest;
-	// Fill before
-	for(i = 0; i < startindex; i++){
-		newarray->array[i] = array->array[i];
-	}
-	// Fill after
-	i = startindex + greatest;
-	j = startindex;
-	while(i < array->size){
-		newarray->array[j] = array->array[i];
-		j++;
-		i++;
-	}
-	print_array(newarray);
-	return newarray;
+array_t* find_largest_sub_array(array_t* array, int* sub_array_size, int* sub_array_start){
+    int cur_length = 1;
+    int i;
+    *sub_array_size = 1;
+    *sub_array_start = 0;
+    // Traverse from second index
+    for(i = 1; i < array->size; i++){
+        if (array->array[i] > array->array[i-1]){
+            cur_length++;
+        } else {
+            // Update if we found new max
+            if (*sub_array_size < cur_length){
+                *sub_array_size = cur_length;
+                *sub_array_start = i - *sub_array_size;
+            }
+            // Reset length for next check
+            cur_length = 1;
+        }
+    }
+    // Check the last length
+    if (*sub_array_size < cur_length){
+        *sub_array_size = cur_length;
+        *sub_array_start = i - *sub_array_size;
+    }
+    // Return the newfound array
+    array_t* sub_array = new_array(*sub_array_size);
+    for(i = 0; i < sub_array->size; i++){
+        sub_array->array[i] = array->array[*sub_array_start + i];
+    }
+    return sub_array;
 }
 
 /*
-  A sorting algorithm based on
-  yahtzee. It randomly shuffles
-  the array and extracts the largest
-  sequential part and merges that into the
-  finished array, then repeat until the
-  whole array is sorted.
+  Function to remove the sub array and return the remaining one.
 */
+array_t* get_remaining_array(array_t* array, int sub_size, int sub_start){
+    array_t* new = new_array(array->size - sub_size);
+    int i = 0;
+    int n;
+    for(n = 0; n < new->size; n++){
+        if(i == sub_start){
+            i += sub_size;
+        }
+        new->array[n] = array->array[i];
+        i++;
+    }
+    dealloc_array(array);
+    return new;
+}
+
+/*
+  Function to merge two sorted arrays and return
+  the resulting array.
+*/
+array_t* merge_sorted_arrays(array_t* a, array_t* b){
+    // Make space for the combined arrays
+    array_t* merged = new_array(a->size + b->size);
+
+    // Loop over both and insert smallest all the time.
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while((i < a->size) && (j < b->size)){
+        if (a->array[i] < b->array[j]){
+            merged->array[k++] = a->array[i++];
+        } else {
+            merged->array[k++] = b->array[j++];
+        }
+    }
+    // Insert whatever remains.
+    while(i < a->size){
+        merged->array[k++] = a->array[i++];
+    }
+    while(j < b->size){
+        merged->array[k++] = b->array[j++];
+    }
+    dealloc_array(a);
+    return merged;
+}
+
+/*
+  Yahtzee-sort algorithm. 
+*/
+array_t* yahtzee_sort(array_t* remaining){
+    int start_size = remaining->size;
+    int cur_size = 0;
+    array_t* result = new_array(0);
+    int sub_size = 0;
+    int sub_start = 0;
+    array_t* sub;
+    do{
+        shuffle(remaining);
+        sub = find_largest_sub_array(remaining, &sub_size, &sub_start);
+        remaining = get_remaining_array(remaining, sub_size, sub_start);
+        result = merge_sorted_arrays(result, sub);
+        cur_size = result->size;
+        dealloc_array(sub);
+    } while(cur_size < start_size);
+
+    dealloc_array(remaining);
+    return result;
+}
+
 int main(int argc, char const *argv[])
 {
-	// Need the exact number of arguments
-	if(argc != 2){
-		printf("Usage:\nyahtzeesort <size of array>");
-		return 1;
-	}
-	container_t *array = malloc(sizeof(container_t));
-	array->size = atoi(argv[1]);
-	printf("Filling array of %d with random numbers\n", array->size);
-	array->array = malloc(sizeof(int)*array->size);
-	// Seed the generator with time
-	srand((unsigned int)time(NULL));
-	// Populate the array with random int.
-	int i;
-	for(i = 0; i < array->size; i++){
-		array->array[i] = randint(0, array->size);
-	}
-	printf("Original array\n");
-	print_array(array);
-	// Find the largest sequential part of the array
-	while (array->size > 0){
-		shuffle(array);
-		array = extract_sequence(array);
+    int size;
+    if(argc != 2){
+        printf("Usage:\n./yahtzee <size of array>\n");
+        return 0;
+    } else {
+        size = (int)strtoul(argv[1], NULL, 10);
+    }
 
-	}
-	return 0;
+    // Allocate space and populate the initial array with random int.
+    array_t* original_array = new_array(size);
+    srand((unsigned int)time(NULL));
+    int i;
+    for(i = 0; i < original_array->size; i++){
+        original_array->array[i] = randint(0, original_array->size - 1);
+    }
+
+    printf("original array:");
+    print_array(original_array);
+
+    array_t* result = yahtzee_sort(original_array);
+
+    printf("sorted array:");
+    print_array(result);
+
+    // Cleanup
+    dealloc_array(result);
+    return 0;
 }
